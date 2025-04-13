e2f10b7f30a899f1ee50b0aa81970e4d15684704
SamuelGuedestheonly <samuca332002@gmail.com>
2025-04-13 14:48:18 -0300

    Create Q6.java
    q6

---
+++ Q6.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner teclado = new
+Scanner(System.in);
+    System.out.print("Digite o salario:");
+    double salario = teclado.nextDouble();
+    double imposto = salario * 0.10;
+    double salariofinal = salario - imposto;
+    System.out.println("salario =" + salario);
+    System.out.println("imposto =" + imposto);
+    System.out.println("salario final =" + salariofinal);
+    
+    }
+}
